3d6ee5d0876fc26fa1bb3bda68765a7869b8f92d
Adarley <adarleyunb@gmail.com>
2017-12-04 02:14:22 -0200

    Correção COPY
    Correção do COPY, output de espelho de memória após execução de simulação.

--- carregador/src/main.cpp
+++ carregador/src/main.cpp
@@ -67,12 +67,15 @@ int main(int argc, const char * argv[]) {
             //COPY, dois operandos
             nextPos = codigo.find(" ",pos);
             string operando = codigo.substr(pos,nextPos-pos);
+            int operandoInt = stoi(operando);
             pos = nextPos + 1;
-            cout << "COPY: " << operando << ", ";
+            cout << "COPY: " << memoria[operandoInt] << ", ";
             nextPos = codigo.find(" ",pos);
             operando = codigo.substr(pos,nextPos-pos);
+            int operandoInt2 = stoi(operando);
             pos = nextPos + 1;
-            cout << operando << endl;
+            cout << memoria[operandoInt2] << endl;
+            memoria[operandoInt2] = memoria[operandoInt];
         }
         else {
             //um operando
@@ -147,7 +150,11 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
-    cout << "---- Fim Simulação:" << endl;
+    cout << "---- Fim Simulação." << endl << "Espelho de memória após execução:" << endl;
+    for (int i = 0; i < tamanho; i++) {
+        cout << memoria[i] << " ";
+    }
+    cout << endl << endl;
     
     int chunks = std::atoi(argv[2]);
     int tamanhoDisponivelMem = 0;
@@ -159,7 +166,7 @@ int main(int argc, const char * argv[]) {
             chunkUnica = i;
         }
     }
-    std::cout << "Memória disponível em bytes:\t" << tamanhoDisponivelMem << std::endl;
+    std::cout << "Memória disponível em bytes para execução:\t" << tamanhoDisponivelMem << std::endl;
     
     if (tamanhoDisponivelMem >= tamanho) {
         //tudo certo, carregar programa em memoria
@@ -217,7 +224,7 @@ int main(int argc, const char * argv[]) {
                     tamanhoEscrito++;
                 }
             }
-            cout << "Código Final: " << codigoNovo << endl;
+            cout << "Código após correção de endereços: " << endl << codigoNovo << endl << endl;
         }
     }
     else {
@@ -247,4 +254,3 @@ size_t find_nth(const string& texto, size_t pos, const string& needle, size_t nt
     if(0 == nth || string::npos == found_pos)  return found_pos;
     return find_nth(texto, found_pos+1, needle, nth-1);
 }
-
